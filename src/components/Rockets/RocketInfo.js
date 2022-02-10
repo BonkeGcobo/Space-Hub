25e55b2bb6e9705f402d7d47c8fe65f6b7ab2a4c
BonkeG <bonkegcobo291195@gmail.com>
2022-02-10 14:47:49 +0200

    Add the reserve actions

--- src/components/Rockets/RocketInfo.js
+++ src/components/Rockets/RocketInfo.js
@@ -2,28 +2,57 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './Rocket.css';
 
-function RocketInfo({ rocket_name: name, rocket_image: rocketImage, description }) {
+function RocketInfo({
+  id,
+  rocket_name: name,
+  rocket_image: rocketImage,
+  description,
+  reserved,
+  handleToggleClick,
+}) {
   return (
     <div className="rocket">
       <img src={rocketImage} alt="rocket" width="250" />
       <div className="aboutRocket">
         <h2 className="rocket-name">{name}</h2>
-        <p className="rocket-descrip">{description}</p>
-        <button
-          type="button"
-          className="rocket-btn"
-        >
-          Reserve Rocket
-        </button>
+        <p className="reserve-sect">
+          {reserved && (
+            <span className="rev-word">
+              Reserved
+            </span>
+          )}
+          {' '}
+          {description}
+        </p>
+        {reserved ? (
+          <button
+            type="button"
+            className="py-2 px-5 rounded text-red-500 border border-red-400"
+            onClick={() => handleToggleClick(id)}
+          >
+            Cancel Reserve
+          </button>
+        ) : (
+          <button
+            type="button"
+            className="rocket-btn "
+            onClick={() => handleToggleClick(id)}
+          >
+            Reserve Rocket
+          </button>
+        )}
       </div>
     </div>
   );
 }
 
 RocketInfo.propTypes = {
+  id: PropTypes.number.isRequired,
   rocket_name: PropTypes.string.isRequired,
   rocket_image: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
+  handleToggleClick: PropTypes.func.isRequired,
+  reserved: PropTypes.bool.isRequired,
 };
 
 export default RocketInfo;
